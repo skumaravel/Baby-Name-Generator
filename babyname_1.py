03fdf434f7425e0c0937143f3580df10c82fa323
skumaravel <skumara@g.clemson.edu>
2018-05-25 17:10:27 -0400

    Added a comment explaining why we imported string module

--- babyname_1.py
+++ babyname_1.py
@@ -1,4 +1,8 @@
+# Importing string module to reuse functions and constants from it
 import string
 
 print(string.ascii_letters)
 print(string.ascii_lowercase)
+
+import random
+print(random.choice(string.ascii_letters))
