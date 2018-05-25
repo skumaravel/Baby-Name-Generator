37fe7a4f2dd7c995b380c8cc67c42558f6a74723
skumaravel <skumara@g.clemson.edu>
2018-05-25 17:11:40 -0400

    Added a comment explaining why we import random module.

--- babyname_1.py
+++ babyname_1.py
@@ -4,5 +4,6 @@ import string
 print(string.ascii_letters)
 print(string.ascii_lowercase)
 
+# Importing random module because we are going to randomly generate names.
 import random
 print(random.choice(string.ascii_letters))
